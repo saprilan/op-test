3a13d893e953c51baf5f43e0d896e39127cc43a1
saprilan <saprilan15@gmail.com>
2025-06-26 07:53:01 +0700

    intial commit

---
+++ src/main/java/com/sapril/op/op/test/OpTestApplication.java
@@ -0,0 +1,13 @@
+package com.sapril.op.op.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sapril/op/op/test/OpTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sapril.op.op.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
